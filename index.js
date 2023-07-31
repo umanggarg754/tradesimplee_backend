90429d715a9bfcb6b917cdc663070b5b7c9659f4
umanggarg754 <umanggarg754@gmail.com>
2023-07-31 20:45:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require("./app/routes/user.routes")(app);
+
+const app = require('./server');
+
+// require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
